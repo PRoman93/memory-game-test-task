3354b5cbf2f7efee87ea9cd0bdd0fcbf3cd52c6a
PRoman93 <roman_prokopets@mail.ua>
2020-08-19 10:18:16 +0300

    Updates

---
+++ precache-manifest.7f7e73e418dc14305b9678c6fb7f869a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3231e0b71a327ebabf13327864d209ef",
+    "url": "/memory-game-test-task/index.html"
+  },
+  {
+    "revision": "b92797b60919ca114554",
+    "url": "/memory-game-test-task/static/css/main.832c9dfe.chunk.css"
+  },
+  {
+    "revision": "981bd459a7f99b730351",
+    "url": "/memory-game-test-task/static/js/2.501a066c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/memory-game-test-task/static/js/2.501a066c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b92797b60919ca114554",
+    "url": "/memory-game-test-task/static/js/main.697da495.chunk.js"
+  },
+  {
+    "revision": "31994f922d3ac3fce682",
+    "url": "/memory-game-test-task/static/js/runtime-main.6d193878.js"
+  },
+  {
+    "revision": "58e2ca2fffa28968daf24b6df198f0b6",
+    "url": "/memory-game-test-task/static/media/Dawn.58e2ca2f.mp3"
+  },
+  {
+    "revision": "f314c1fd7cc42567e61c3ff17c0989d1",
+    "url": "/memory-game-test-task/static/media/Sound_20947.f314c1fd.mp3"
+  },
+  {
+    "revision": "d4e381331a27da2957977ae93f3e10cf",
+    "url": "/memory-game-test-task/static/media/austria.d4e38133.svg"
+  },
+  {
+    "revision": "817a309b4347513ec64a861b7a9d37b2",
+    "url": "/memory-game-test-task/static/media/belarus.817a309b.svg"
+  },
+  {
+    "revision": "07d9212683b135a684eb2492ca8f0ffc",
+    "url": "/memory-game-test-task/static/media/belgium.07d92126.svg"
+  },
+  {
+    "revision": "4fc1c111358ba3d6e689a7f9d3957089",
+    "url": "/memory-game-test-task/static/media/brazil.4fc1c111.svg"
+  },
+  {
+    "revision": "5e845b41e05e0a24cc3be6347fe44f33",
+    "url": "/memory-game-test-task/static/media/britain.5e845b41.svg"
+  },
+  {
+    "revision": "429827e685856f650825235f75142fe8",
+    "url": "/memory-game-test-task/static/media/click.429827e6.mp3"
+  },
+  {
+    "revision": "36d6d563fcdc334adfae041f6d0535ba",
+    "url": "/memory-game-test-task/static/media/france.36d6d563.svg"
+  },
+  {
+    "revision": "ae5b37f5982dcf8f2c4a1c1fa0dfc382",
+    "url": "/memory-game-test-task/static/media/japan.ae5b37f5.svg"
+  },
+  {
+    "revision": "8953ff1ae1539773bcdbf1a6fb69c9d2",
+    "url": "/memory-game-test-task/static/media/spain.8953ff1a.svg"
+  }
+]);
